ec9c15724a1d32b40ec1b6730e947ef23e7ebb8c
Léo Theytaz <leo.theytaz@secutix.com>
2024-04-23 15:34:00 +0200

    smart-money contract
    to be reviewed

---
+++ contracts/smart-money/smart-money.sol
@@ -0,0 +1,58 @@
+pragma solidity 0.8.20;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract SmartMoney is Ownable(msg.sender) {
+    // Define the mapping from orderID to order details
+    mapping(string => Order) public orders;
+    address public shopAddress;
+
+    struct Order {
+        bool verified;
+        uint received;
+        uint price;
+        bool paid;
+        address payable sender; // wallet from which the last payment originated - TODO see if needed to send back the money
+    }
+
+    // Initialize orders/payments with orderID and price to be received
+    function initializeOrder(string memory orderID, uint price) external onlyOwner {
+        require(orders[orderID].price == 0, "Order already exists");
+        orders[orderID].price = price;
+    }
+
+    // Receive payment for an order
+    function pay(string memory orderID) external payable {
+        require(orders[orderID].price > 0, "Order does not exist");
+
+        // Update the received amount
+        orders[orderID].received += msg.value;
+        orders[orderID].sender = payable(msg.sender);
+
+        // Check if received amount exceeds price, and update verified status accordingly
+        if (orders[orderID].received >= orders[orderID].price) {
+            orders[orderID].paid = true;
+            conditionalOutput(orderID, false);
+        }
+    }
+
+    // Notification function for the verification status of the order
+    function notify(string memory orderID, bool verified) external onlyOwner {
+        require(orders[orderID].price > 0, "Order does not exist");
+        orders[orderID].verified = verified;
+        conditionalOutput(orderID, true);
+    }
+
+    function conditionalOutput(string memory orderID, bool fromVerify) view  internal  {
+        if (fromVerify && !orders[orderID].verified) {
+            // Send back received money so-far to the sender
+            orders[orderID].sender.transfer(orders[orderID].received);
+            delete orders[orderID];
+        }
+        if (orders[orderID].paid && orders[orderID].verified) {
+            // Cash-out to the shop's account
+            payable(shopAddress).transfer(orders[orderID].received);
+            delete orders[orderID];
+        }
+    }
+}
