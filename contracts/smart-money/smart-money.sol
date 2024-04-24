569a623ab3ec14fbc5d3d273de9e907e0783ffbb
Léo Theytaz <leo.theytaz@secutix.com>
2024-04-24 14:00:51 +0200

    simplified payment logic smart-money.sol

--- contracts/smart-money/smart-money.sol
+++ contracts/smart-money/smart-money.sol
@@ -9,17 +9,19 @@ contract SmartMoney is Ownable(msg.sender) {
     mapping(uint256 => Order) public orders;
     address public shopAddress = 0x40775600Bb4E2E4Ab1c24B5c8bA4734cC47EE02E;
 
+    function setShopAddress(address _shopAddress) external onlyOwner {
+        shopAddress = _shopAddress;
+    }
+
     struct Order {
         bool verified;
-        uint256 received;
         uint256 price;
         bool paid;
-        address payable sender; // wallet from which the last payment originated - TODO see if needed to send back the money
     }
 
     // Initialize orders/payments with orderID and price to be received
-    function initializeOrder(uint256 orderID, uint256 price) // TODO check if only from the shop smart account
-        external
+    function initializeOrder(uint256 orderID, uint256 price)
+    external
     {
         require(orders[orderID].price == 0, "Order already exists");
         orders[orderID].price = price;
@@ -27,17 +29,8 @@ contract SmartMoney is Ownable(msg.sender) {
 
     // Receive payment for an order
     function pay(uint256 orderID) external payable {
-        require(orders[orderID].price > 0, "Order does not exist");
-
-        // Update the received amount
-        orders[orderID].received += msg.value;
-        orders[orderID].sender = payable(msg.sender);
-
-        // Check if received amount exceeds price, and update verified status accordingly
-        if (orders[orderID].received >= orders[orderID].price) {
-            orders[orderID].paid = true;
-            conditionalOutput(orderID, false);
-        }
+        require(orders[orderID].price == msg.value, "Order does not exist or price doesn't match");
+        orders[orderID].paid = true;
     }
 
     // Notification function for the verification status of the order
@@ -48,16 +41,15 @@ contract SmartMoney is Ownable(msg.sender) {
     }
 
     function conditionalOutput(uint256 orderID, bool fromVerify)
-        internal
+    internal
     {
         if (fromVerify && !orders[orderID].verified) {
-            // Send back received money so-far to the sender
-            orders[orderID].sender.transfer(orders[orderID].received);
+            // Cancel the order
             delete orders[orderID];
         }
         if (orders[orderID].paid && orders[orderID].verified) {
             // Cash-out to the shop's account
-            payable(shopAddress).transfer(orders[orderID].received);
+            payable(shopAddress).transfer(orders[orderID].price);
             delete orders[orderID];
         }
     }
