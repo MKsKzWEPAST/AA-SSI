6139b2a8c1843eb6d199ee06400d6a30978b279f
Léo Theytaz <leo.theytaz@secutix.com>
2024-04-26 15:53:50 +0200

    Pay with DAI & USDT (to be tested)

--- contracts/smart-money/smart-money.sol
+++ contracts/smart-money/smart-money.sol
@@ -3,6 +3,7 @@
 pragma solidity 0.8.20;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract SmartMoney is Ownable(msg.sender) {
 
@@ -32,10 +33,27 @@ contract SmartMoney is Ownable(msg.sender) {
         orders[orderID].verified = !ageRequired;
     }
 
-    // Receive payment for an order
+    // Receive payment for an order - TODO - remove function when no longer in "development"
     function pay(uint256 orderID) external payable {
-        require(orders[orderID].price == msg.value && msg.value != 0, "Order does not exist or price doesn't match");
+        require(orders[orderID].price == msg.value && msg.value != 0, "Order does not exist or price doesn't match [ETH]");
         require(!orders[orderID].paid, "Order already paid");
+        orders[orderID].paid = true;
+        conditionalOutput(orderID, false);
+    }
+
+    function payErc20(uint256 orderID, address _token, uint256 amount) external {
+        require(orders[orderID].price == amount && amount>0, "Order does not exist or price doesn't match [ERC20]");
+        require(!orders[orderID].paid, "Order already paid [ERC20]");
+
+        IERC20 tokenContract = IERC20(_token);
+
+        uint256 allowance = tokenContract.allowance(msg.sender, address(this));
+        require(allowance >= amount, "Insufficient allowance [ERC20]");
+
+        // Attempt to transfer tokens from the sender to this contract
+        bool success = tokenContract.transferFrom(msg.sender, address(this), amount);
+        require(success, "Token transfer failed [ERC20]");
+
         orders[orderID].paid = true;
         conditionalOutput(orderID, false);
     }
