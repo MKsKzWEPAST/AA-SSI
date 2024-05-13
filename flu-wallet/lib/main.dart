566241615630cd8071ab0e8305247feb530090eb
NazianzenovSec <victor.nazianzeno-lejamtel@secutix.com>
2024-05-13 13:14:29 +0200

    dependency validator

--- flu-wallet/lib/main.dart
+++ flu-wallet/lib/main.dart
@@ -4,7 +4,6 @@ import 'package:polygonid_flutter_sdk/sdk/polygon_id_sdk.dart';
 import 'package:wallet_app/src/presentation/dependency_injection/dependencies_provider.dart'
     as di;
 import 'package:firebase_core/firebase_core.dart';
-import 'package:wallet_app/utils/auth_model.dart';
 import 'firebase_options.dart';
 import 'package:wallet_app/src/presentation/app.dart';
 import 'dart:async';
@@ -12,13 +11,14 @@ import 'dart:async';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  //initializes firebase for login
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
   //Dependency Injection initialization
   await di.init();
   PolygonIdSdk.I.switchLog(enabled: true);
-
 
   // App UI locked in portrait mode
   await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
